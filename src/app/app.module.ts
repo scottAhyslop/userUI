1e837c69a2537b131cacdd0ddbf6d1bb807b364f
Scott A. Hyslop <sahyslop@gmail.com>
2021-12-10 20:43:33 -0500

    init commit on Angular 13 front-end to match .net 5 webAPI, initiated project, created app.component.html landing page with some nav and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeviceListComponent } from './components/device-list/device-list.component';
+import { DeviceComponent } from './components/device/device.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceListComponent,
+    DeviceComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
